f9ff87f2a22a75816f650ab3e08ca26bb2933c8d
Viktor Vallmark <vallmark.viktor@gmail.com>
2024-09-10 09:32:37 +0200

    Diagram for task1

---
+++ Uppgift2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Uppgift2 {
+
+  public static void main(String[] args) {
+
+    int first, second, third;
+    Scanner scan = new Scanner(System.in);
+  }
+}
