05c3c8ce743bfc9d05c6f8c5b62c46c0be489f9d
Viktor Vallmark <vallmark.viktor@gmail.com>
2024-09-10 10:22:50 +0200

    Partial solution to uppgift2

--- Uppgift2.java
+++ Uppgift2.java
@@ -3,8 +3,34 @@ import java.util.Scanner;
 public class Uppgift2 {
 
   public static void main(String[] args) {
-
-    int first, second, third;
+    // Variable declarations
+    int first, second, third, largest;
     Scanner scan = new Scanner(System.in);
+    int[] arr = new int[3];
+
+    // Getting user input
+    System.out.println("Enter first number: ");
+    first = scan.nextInt();
+    System.out.println("Enter second number: ");
+    second = scan.nextInt();
+    System.out.println("Enter third number: ");
+    third = scan.nextInt();
+
+    arr[0] = first;
+    arr[1] = second;
+    arr[2] = third;
+    largest = 0;
+    scan.close();
+    for (int num : arr) {
+      if (num > largest) {
+        largest = num;
+      }
+    }
+
+    for (int num : arr) {
+      if (largest == num) {
+        continue;
+      }
+    }
   }
 }
