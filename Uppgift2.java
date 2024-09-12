665ad106dfc186240336283980c81746d5859a4b
Viktor Vallmark <vallmark.viktor@gmail.com>
2024-09-12 13:04:14 +0200

    Finished Uppgift2

--- Uppgift2.java
+++ Uppgift2.java
@@ -2,9 +2,18 @@ import java.util.Scanner;
 
 public class Uppgift2 {
 
+  private static boolean compare(int first, int second) {
+
+    if (first == second) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   public static void main(String[] args) {
 
-    int first, second, third, largest, index;
+    int first, second, third, largest, smallest, index, count;
     Scanner scan = new Scanner(System.in);
     int[] arr = new int[3];
 
@@ -16,14 +25,34 @@ public class Uppgift2 {
     System.out.println("Enter third number: ");
     third = scan.nextInt();
 
+    scan.close();
+
     arr[0] = first;
     arr[1] = second;
     arr[2] = third;
     largest = first;
+    smallest = first;
+    count = 0;
 
-    for (index = 0; index < arr.length; index++) {
+    for (index = 1; index < arr.length; index++) {
+
+      if (arr[index] > largest) {
+        largest = arr[index];
+      } else if (arr[index] == largest) {
+        count++;
+      } else if (arr[index] < smallest) {
+        smallest = arr[index];
+      }
     }
-
-    scan.close();
+    // Blursed comparisons
+    if ((compare(first, second)) && (compare(second, third))) {
+      System.out.println("The numbers are the same.");
+    } else if (((compare(first, second)) || (compare(first, third)) || (compare(second, third)))
+        && ((smallest < first) && (smallest < second) && (smallest < third))) {
+      System.out.println(
+          "Two numbers are the same and third is smaller. Smallest number is: " + smallest);
+    } else if (count < 1) {
+      System.out.println("One number is bigger than the rest and it's: " + largest);
+    }
   }
 }
